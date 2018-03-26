382579fe046ad7d71dddb9552b910d25a6d56874
Lepeshka92 <Lepeshka92@users.noreply.github.com>
2018-03-26 20:39:31 +0300

    Update api.py

--- api.py
+++ api.py
@@ -2,17 +2,21 @@ import urequests as requests
 import ujson as json
 
 
-class TelegramBot:
-    
+class TelegramBot(object):
+
+
     def __init__(self, token):
         self.token = token
         self.offset = 0
         self._url = 'https://api.telegram.org/bot' + token
 
+    def _quote(self, t):
+        return '%'.join('{:02x}'.format(c) for c in t)
+
     def send(self, chat_id, text):
-        data = 'chat_id={}&text={}'.format(chat_id, text)
-        url = self._url + '/sendMessage?' + data.replace(' ', '%20')
-        
+        url = self._url + '/sendMessage?chat_id=' + str(chat_id) + \
+              '&text=%' + self._quote(text.encode('utf-8'))
+
         try:
             requests.get(url)
             return True
@@ -20,22 +24,23 @@ class TelegramBot:
             return False
 
     def update(self):
-        data = 'timeout=30&limit=1&offset=' + str(self.offset)
-        url = self._url + '/getUpdates?' + data
-        
+        url = self._url + '/getUpdates?timeout=30&limit=1&offset=' + \
+              str(self.offset)
+
         try:
             r = requests.get(url)
             jo = json.loads(r.text)
         except:
             return None
-            
+
         if len(jo['result']) > 0:
             self.offset = jo['result'][0]['update_id'] + 1
             if 'text' in jo['result'][0]['message']:
                 return (jo['result'][0]['message']['chat']['id'],
                         str(jo['result'][0]['message']['from']['first_name']),
-                        str(jo['result'][0]['message']['text']))
-        
+                        str(jo['result'][0]['message']['text']),
+                        jo['result'][0]['message']['date'])
+
         return None
 
     def listen(self, handler):
@@ -43,5 +48,3 @@ class TelegramBot:
             message = self.update()
             if message:
                 handler(message)
-
-
