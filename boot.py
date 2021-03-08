3e97dca1560ec3efca81aaf572382a421ea50c40
Lepeshka92 <Lepeshka92@users.noreply.github.com>
2021-03-08 14:58:41 +0300

    Update boot.py

--- boot.py
+++ boot.py
@@ -1,15 +1,22 @@
-import gc
 import network
 
 
-ap_if = network.WLAN(network.AP_IF)
-ap_if.active(False)
+aps = dict()
+aps['Wifi'] = 'password'
+aps['Wifi'] = 'password'
+aps['Wifi'] = 'password'
 
 sta_if = network.WLAN(network.STA_IF)
+
 if not sta_if.isconnected():
     sta_if.active(True)
-    sta_if.connect('WIFI', 'PASSWORD')
-    while not sta_if.isconnected():
-        pass
-
-gc.collect()
+    networks = sta_if.scan()
+    for net in networks:
+        ssid = net[0].decode()
+        if ssid in aps:  
+            sta_if.connect(ssid, aps[ssid])
+            while not sta_if.isconnected():
+                pass
+            break
+del aps
+print('network config:', sta_if.ifconfig())
